b219c032cf2849dc1fb0687e63dd42233df83b3a
GabrielKitanovich <gabriel.kitanovich@gmail.com>
2025-08-31 22:35:46 -0300

    Add source link and PDB files for debugging support in WebApplication1

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,7 +1,18 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<ITaskService>(new InMemoryTaskService());
+
 var app = builder.Build();
+
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"Request: {context.Request.Method} {context.Request.Path} at {DateTime.Now} Started");
+    await next(context);
+    Console.WriteLine($"Response: {context.Response.StatusCode} at {DateTime.Now} Finished");
+});
 
 var todos = new List<Todo>
 {
@@ -9,33 +20,85 @@ var todos = new List<Todo>
     new(2, "Build a web app", DateTime.Now.AddDays(14), false)
 };
 
-app.MapGet("/todos", () => todos);
+app.MapGet("/todos", (ITaskService taskService) => taskService.GetTodos());
 
-app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id, ITaskService taskService) =>
 {
-    var todo = todos.FirstOrDefault(t => t.Id == id);
+    var todo = taskService.GetTodoById(id);
     return todo is not null
     ? TypedResults.Ok(todo)
     : TypedResults.NotFound();
 });
 
-app.MapPost("/todos", (Todo todo) =>
+app.MapPost("/todos", (Todo todo, ITaskService taskService) =>
 {
-    todos.Add(todo);
+    taskService.AddTodo(todo);
     return TypedResults.Created($"/todos/{todo.Id}", todo);
+}).AddEndpointFilter(async (context, next) => {
+    var taskArgument = context.GetArgument<Todo>(0);
+    var errors = new Dictionary<string, string[]>();
+    if (taskArgument.DueDate < DateTime.UtcNow)
+    {
+        errors.Add(nameof(Todo.DueDate), ["Cannot have due date in the past."]);
+    }
+    if (taskArgument.IsCompleted)
+    {
+        errors.Add(nameof(Todo.IsCompleted), ["Cannot add completed todo."]);
+    }
+
+    if (errors.Count > 0)
+    {
+        return Results.ValidationProblem(errors);
+    }
+
+    return await next(context);
 });
 
-app.MapDelete("/todos/{id}", Results<NoContent, NotFound> (int id) =>
+app.MapDelete("/todos/{id}", Results<NoContent, NotFound> (int id, ITaskService taskService) =>
 {
-    var todo = todos.FirstOrDefault(t => t.Id == id);
+    var todo = taskService.GetTodoById(id);
     if (todo is null)
     {
         return TypedResults.NotFound();
     }
-    todos.Remove(todo);
+    taskService.DeleteTodoById(id);
     return TypedResults.NoContent();
 });
 
 app.Run();
 
 public record Todo(int Id, string Name, DateTime DueDate, bool IsCompleted);
+
+interface ITaskService
+{
+    Todo? GetTodoById(int id);
+    List<Todo> GetTodos();
+    void DeleteTodoById(int id);
+    Todo AddTodo(Todo todo);
+}
+
+class InMemoryTaskService : ITaskService
+{
+    private readonly List<Todo> _todos = [];
+
+    public Todo AddTodo(Todo task)
+    {
+        _todos.Add(task);
+        return task;
+    }
+
+    public void DeleteTodoById(int id)
+    {
+        _todos.RemoveAll(task => id == task.Id);
+    }
+
+    public Todo? GetTodoById(int id)
+    {
+        return _todos.SingleOrDefault(t => id == t.Id);
+    }
+
+    public List<Todo> GetTodos()
+    {
+        return _todos;
+    }
+}
--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d871fc678c5d506d136d69c49d45f8e8dbd3f6f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
